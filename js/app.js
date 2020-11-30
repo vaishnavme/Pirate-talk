98de95731a2ceaa5ec3a7ebc27d69c70f8da10a0
vaishnavme <vaishnavchandurkar21@outlook.com>
2020-11-30 14:42:10 +0530

    Update Style and Added js features

---
+++ js/app.js
@@ -0,0 +1,50 @@
+// input data
+const textInput = document.querySelector("#text-input");
+
+// output place
+const outputPlace = document.querySelector("#text-output");
+
+// spinning animation 
+const loading = document.querySelector("#spinner");
+
+// btn
+const buttonTranslate = document.querySelector("#btn-translate");
+
+buttonTranslate.addEventListener("click", translateHandler);
+
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+// functional part
+
+function showSpinner() {
+    loading.className = "show";
+    setTimeout(() => {
+        loading.className = loading.className.replace("show", "");
+    }, 5000);
+}
+
+function hideSpinner() {
+    loading.className = loading.className.replace("show", "");
+}
+
+function errorHandler(error) {
+    hideSpinner()
+    outputPlace.innerHTML = "Error " + error;
+}
+function translateHandler(event) {
+    showSpinner()
+    var inputData = textInput.value;
+    var finalURL = buildURL(inputData);
+
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            hideSpinner()
+            outputPlace.innerText = json.contents.translated;
+        })
+        .catch(errorHandler)
+}
+
+function buildURL(inData) {
+    return `${url}?text=${inData}`;
+}
